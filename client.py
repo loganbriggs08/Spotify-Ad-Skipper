d750fcc513d3bb5fb805eb7a8568a1ced9be8f8d
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-06-21 22:29:10 +0100

    Import Program from modules and use the is_running function

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
+from modules import Program
+
 class Client:
     def __init__(self):
-        print("Created instance")
+        Program.is_running("Spotify.exe")
     
 if __name__ == "__main__":
     Client_Instance: object = Client()
