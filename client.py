97a3e148dd9bb9e1e9a1b7f913e8843c7ef225a4
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-06-21 22:36:26 +0100

    Store is_running_result as bool then print

--- client.py
+++ client.py
@@ -1,8 +1,11 @@
 from modules import Program
 
 class Client:
-    def __init__(self):
-        Program.is_running("Spotify.exe")
-    
+    def __init__(self, process: str):
+        self.process: str = process
+        
+        Is_Running_Result: bool = Program.is_running(task_name=process)
+        print(Is_Running_Result)
+
 if __name__ == "__main__":
-    Client_Instance: object = Client()
+    Client_Instance: object = Client(process="Spotify.exe")
