c9bbf211f82af714445f9f6a58ad208fa7613682
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-06-21 22:43:16 +0100

    Create a custom type of errors

--- client.py
+++ client.py
@@ -1,11 +1,13 @@
+from typing import Union 
 from modules import Program
 
+error_type: type = Union[None, dict[any]]
+
 class Client:
-    def __init__(self, process: str):
+    def __init__(self, process: str) -> error_type:
         self.process: str = process
         
         Is_Running_Result: bool = Program.is_running(task_name=process)
-        print(Is_Running_Result)
 
 if __name__ == "__main__":
     Client_Instance: object = Client(process="Spotify.exe")
