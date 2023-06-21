591751ce398b85692e370230283d5281401594f7
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-06-21 22:22:29 +0100

    Create Program class and is_running function

---
+++ modules/program.py
@@ -0,0 +1,10 @@
+class Program:
+    def is_running(task_name: str) -> bool:
+        """Check if a program is running and return True/False.
+
+        Args:
+            task_name (str): The name the program should be running as.
+
+        Returns:
+            bool: If program is running or not
+        """
