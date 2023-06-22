ef8241f0da25353742de25fa92c78e4d042210fd
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-06-22 19:53:49 +0100

    Create information function to get information about the process

--- modules/program.py
+++ modules/program.py
@@ -1,5 +1,7 @@
 import psutil
 
+from typing import Union
+
 class Program:
     def is_running(task_name: str) -> bool:
         """Check if a program is running and return True/False.
@@ -24,3 +26,19 @@ class Program:
                     return True
                 
             return False
+        
+    def information(task_name: str) -> Union[None, object]:
+        if task_name.endswith(".exe"):
+            for program in psutil.process_iter():
+                if program.name() == task_name:
+                    return program
+                
+            return None
+                
+        else:
+            for program in psutil.process_iter():
+                if program.name() == f"{task_name}.exe":
+                    return program
+                
+            return None
+        
