9aa26d4746b43d370ea019f561d45661c03eb70f
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-06-22 21:12:01 +0100

    Add success function to logging.py

--- modules/logging.py
+++ modules/logging.py
@@ -5,3 +5,6 @@ init(convert=True)
 class Logging:
     def error(message: str):
         print(f"{Fore.RED}[ERROR]{Fore.WHITE} {message}")
+        
+    def success(message: str):
+        print(f"{Fore.GREEN}[SUCCESS]{Fore.WHITE} {message}")
