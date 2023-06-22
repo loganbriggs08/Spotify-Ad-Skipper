561b6764200f3b715a58a33b1c1deee2ca6b7378
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-06-22 19:54:01 +0100

    Check if spotify is logging else print error

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 from typing import Union 
-from modules import Program
+from modules import Program, Logging
 
 class Client:
     def __init__(self, process: str) -> None:
@@ -7,7 +7,10 @@ class Client:
         
         is_running: bool = Program.is_running(task_name=process)
         
-        print(is_running)
+        if is_running == True:
+            print("Spotify is running..")
+        else:
+            Logging.error(f"{process} couldn't be found in the process list..")
 
 if __name__ == "__main__":
     Client_Instance: object = Client(process="Spotify.exe")
