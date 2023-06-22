f95804ff62ff53a1e3785061d8c298fc95bdc2f4
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-06-22 21:28:48 +0100

    Change indentation of time.sleep and remove self

--- modules/watcher.py
+++ modules/watcher.py
@@ -5,11 +5,13 @@ import pygetwindow
 from modules import Program, Logging
 
 class Watcher:
-    def start(self) -> None:
+    def start() -> None:
         """Start watching for advertisements to close and then reopen spotify."""
         
         while True:
             for window in pygetwindow.getAllWindows():
+                print(window.title)
+                
                 if window.title == "Advertisement":
                     if Program.is_running("Spotify.exe") == True:
                         Logging.success("Spotify.exe Process found with running AD..")
@@ -18,5 +20,6 @@ class Watcher:
                             Logging.success("Spotify.exe process has been killed successfully."); os.system('cmd /k "spotify.exe"')
                         else:
                             Logging.error("Failed to kill Spotify.exe process..")
+                            
+            time.sleep(0.5)
 
-                time.sleep(0.5)
