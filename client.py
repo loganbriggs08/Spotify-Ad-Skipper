538236e9f50d85aa8f94795854a45c50f9d0b219
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-06-22 19:47:13 +0100

    return None instead of invalid return type

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ from typing import Union
 from modules import Program
 
 class Client:
-    def __init__(self, process: str) -> error_type:
+    def __init__(self, process: str) -> None:
         self.process: str = process
         
         is_running: bool = Program.is_running(task_name=process)
