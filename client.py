5f3c449c08e32646bf98e035a15f91559ac01fa4
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-06-22 19:46:24 +0100

    Remove old error systemn

--- client.py
+++ client.py
@@ -7,15 +7,9 @@ class Client:
     def __init__(self, process: str) -> error_type:
         self.process: str = process
         
-        Is_Running_Result: bool = Program.is_running(task_name=process)
+        is_running: bool = Program.is_running(task_name=process)
         
-        if Is_Running_Result == False:
-            return {"error": f"{process} has to be open for this to work."}
-        else:
-            print("Spotify is running")
+        print(is_running)
 
 if __name__ == "__main__":
     Client_Instance: object = Client(process="Spotify.exe")
-    
-    if Client_Instance is not None:
-        print(f"[ERROR] {Client_Instance}")
