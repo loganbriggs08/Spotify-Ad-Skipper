5362916020eef9574ba06b797d74a210ce8ddcff
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-06-22 21:29:07 +0100

    Remove old logging statements

--- modules/watcher.py
+++ modules/watcher.py
@@ -9,9 +9,7 @@ class Watcher:
         """Start watching for advertisements to close and then reopen spotify."""
         
         while True:
-            for window in pygetwindow.getAllWindows():
-                print(window.title)
-                
+            for window in pygetwindow.getAllWindows():                
                 if window.title == "Advertisement":
                     if Program.is_running("Spotify.exe") == True:
                         Logging.success("Spotify.exe Process found with running AD..")
